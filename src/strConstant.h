885ff7d7e1cb90619702afd5bce86b020e4bdc8b
Stève Jordan KING <jsmking777@gmail.com>
2022-08-24 17:04:24 +0200

    fix

--- src/strConstant.h
+++ src/strConstant.h
@@ -26,7 +26,7 @@ namespace str {
 	namespace app {
 		const static std::string PROTOCOL = "http";
 		const static std::string HOST = "0.0.0.0";
-		const static std::string HOST = "34566";
+		const static std::string PORT = "34566";
 	}
 }
 

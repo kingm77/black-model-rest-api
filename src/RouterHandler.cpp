62f49ada4022d2d7286893b20e10ffbf23a3c6ed
Stève Jordan KING <jsmking777@gmail.com>
2022-11-17 12:26:19 +0100

    Update RouterHandler.cpp

--- src/RouterHandler.cpp
+++ src/RouterHandler.cpp
@@ -5,7 +5,7 @@ RouterHandler::RouterHandler(Router& rtr)
 	router = rtr;
 }
 
-void RouterHandler::handle(std::string& path, const std::string& method, const web::http::http_request& req)
+void RouterHandler::handle(std::string path, const std::string& method, const web::http::http_request& req)
 {
 	router.handle(req, path, method);
 }
